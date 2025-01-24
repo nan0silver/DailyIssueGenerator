89e97ec39a47852d29794835d18229b3be818b88
NaHyun Eun <82145661+nan0silver@users.noreply.github.com>
2025-01-24 18:02:43 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 async function makeIssue() {
     const token = process.env.GH_TOKEN;
-    const OWNER = "nan0silver";
-    const REPO = "github_power_actions"; //현재 레포지토리 이름
+    const OWNER = "nan0silver"; 
+    const REPO = "github_power_actions"; 
     const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
         headers: {
-            Authorization: 'Bearer ${token}'
+            Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-            title: "행운의 숫자",
-            body: '${Math.floor(Math.random() * 100) + 1}',
+            title: "행운의 숫자", 
+            body: `${Math.floor(Math.random() * 100) + 1}`, 
         })
     });
     if (response.ok) {
